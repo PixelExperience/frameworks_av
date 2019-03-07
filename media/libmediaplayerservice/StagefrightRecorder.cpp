dca428945ca77ab0e623fa8ce3e446874f0ccca8
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-07 15:02:00 -0300

    Revert "Stagefright: Do not skip frames in time-lapse-source for high-speed"
    This reverts commit a857264b63fc8edbc37baf7b9ab5dfbf26c0424b.

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1992,12 +1992,9 @@ status_t StagefrightRecorder::resume() {
         if (mPauseStartTimeUs < bufferStartTimeUs) {
             mPauseStartTimeUs = bufferStartTimeUs;
         }
-
-    // 30 ms buffer to avoid timestamp overlap
-    mTotalPausedDurationUs +=
-        resumeStartTimeUs - mPauseStartTimeUs -
-        (30000 * (mCaptureFpsEnable ? (mCaptureFps / mFrameRate):1));
-      }
+        // 30 ms buffer to avoid timestamp overlap
+        mTotalPausedDurationUs += resumeStartTimeUs - mPauseStartTimeUs - 30000;
+    }
     double timeOffset = -mTotalPausedDurationUs;
     if (mCaptureFpsEnable) {
         timeOffset *= mCaptureFps / mFrameRate;
