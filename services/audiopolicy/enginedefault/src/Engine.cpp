ba4968657292facc88aceae8e55088358c0a25aa
evmn <afls@yandex.com>
2019-05-06 20:07:52 +0300

    Add system audio recording support [2/2]
    Source: https://github.com/evmn/hack-android/tree/master/Record_Internal_Audio
    Change-Id: I92cffe4cf27c50a6ab9645afbfe95fbb16c364f6

--- services/audiopolicy/enginedefault/src/Engine.cpp
+++ services/audiopolicy/enginedefault/src/Engine.cpp
@@ -528,11 +528,21 @@ audio_devices_t Engine::getDeviceForStrategyInt(routing_strategy strategy,
             break;
         }
 
-        if (strategy != STRATEGY_SONIFICATION) {
+        if (strategy != STRATEGY_SONIFICATION &&
+                availableOutputDevices.getDevice(AUDIO_DEVICE_OUT_REMOTE_SUBMIX, String8("0")) != 0) {
             // no sonification on remote submix (e.g. WFD)
-            if (availableOutputDevices.getDevice(AUDIO_DEVICE_OUT_REMOTE_SUBMIX,
+            device2 = availableOutputDevices.types() & AUDIO_DEVICE_OUT_REMOTE_SUBMIX;
+            if (availableOutputDevices.getDevice(AUDIO_DEVICE_OUT_WIRED_HEADPHONE,
                                                  String8("0")) != 0) {
-                device2 = availableOutputDevices.types() & AUDIO_DEVICE_OUT_REMOTE_SUBMIX;
+                device2 |= (availableOutputDevicesType & AUDIO_DEVICE_OUT_WIRED_HEADPHONE);
+            }
+            if (availableOutputDevices.getDevice(AUDIO_DEVICE_OUT_WIRED_HEADSET,
+                                                 String8("0")) != 0) {
+                device2 |= (availableOutputDevicesType & AUDIO_DEVICE_OUT_WIRED_HEADSET);
+            }
+            if (availableOutputDevices.getDevice(AUDIO_DEVICE_OUT_SPEAKER,
+                                                 String8("0")) != 0) {
+                device2 |= (availableOutputDevicesType & AUDIO_DEVICE_OUT_SPEAKER);
             }
         }
         if (isInCall() && (strategy == STRATEGY_MEDIA)) {
